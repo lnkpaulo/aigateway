ca38c17bc19301802220928f19f63da640e0b10b
PJP <paulo@weron.net>
2024-10-16 16:37:22 +0100

    Small update.

--- src/gen_api_key.py
+++ src/gen_api_key.py
@@ -1,10 +1,10 @@
 # gen_api_key.py
 import sys
 from libs.token_manager import TokenManager
-from colorama import init, Fore, Style
+# from colorama import init, Fore, Style
 
 # Initialize colorama
-init(autoreset=True)
+# init(autoreset=True)
 
 # Initialize the token manager
 token_manager = TokenManager()
@@ -12,32 +12,32 @@ token_manager = TokenManager()
 def generate_token(user, api_name):
     try:
         token = token_manager.generate_token(user, api_name)
-        print(Fore.GREEN + f"Generated token for {user} with API {api_name}: {token}")
+        print(f"Generated token for {user} with API {api_name}: {token}")
     except ValueError as e:
-        print(Fore.RED + str(e))
+        print(str(e))
 
 def list_users():
     users = token_manager.list_users()
     if users:
-        print(Fore.YELLOW + "Available users and their API KEYs name:")
+        print("Available users and their API KEYs name:")
         for user, api_name in users:
-            print(Fore.CYAN + f"User: {user}, API KEY Name: {api_name}")
+            print(f"User: {user}, API KEY Name: {api_name}")
     else:
-        print(Fore.RED + "No users found.")
+        print("No users found.")
 
 def delete_token(user, api_name):
     try:
         token_manager.revoke_token(user, api_name)
-        print(Fore.GREEN + f"Deleted token for {user} with API {api_name}.")
+        print(f"Deleted token for {user} with API {api_name}.")
     except ValueError as e:
-        print(Fore.RED + str(e))
+        print(str(e))
 
 def main():
     if len(sys.argv) < 2:
-        print(Fore.YELLOW + "Usage:")
-        print(Fore.YELLOW + "  python gen_api_key.py -g <user> <api_key_name>     # Generate token")
-        print(Fore.YELLOW + "  python gen_api_key.py -l                           # List users")
-        print(Fore.YELLOW + "  python gen_api_key.py -d <user> <api_key_name>     # Delete token")
+        print("Usage:")
+        print("  python gen_api_key.py -g <user> <api_key_name>     # Generate token")
+        print("  python gen_api_key.py -l                           # List users")
+        print("  python gen_api_key.py -d <user> <api_key_name>     # Delete token")
         sys.exit(1)
 
     if sys.argv[1] == '-g' and len(sys.argv) == 4:
@@ -51,7 +51,7 @@ def main():
         api_name = sys.argv[3]
         delete_token(user, api_name)
     else:
-        print(Fore.RED + "Invalid command or arguments.")
+        print("Invalid command or arguments.")
         sys.exit(1)
 
 if __name__ == "__main__":
